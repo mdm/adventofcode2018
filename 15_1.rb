5ec5aa85762eecb9148fc7ad322f8af4aa9870da
Marc Dominik Migge <marcmigge@gmx.net>
2020-11-21 14:43:30 +0100

    New implementation for day 15, still buggy

--- 15_1.rb
+++ 15_1.rb
@@ -1,206 +1,238 @@
 require 'pp'
 require 'set'
 
-grid = []
-hitpoints = []
-while row = gets
-    grid << row
-    hitpoints << ([200] * row.size)
+class Unit
+    attr_accessor :team, :x, :y, :hitpoints
 end
 
-width = grid[0].size - 2
-height = grid.size - 2
-
-def mark_targets(grid, unit_x, unit_y)
-    width = grid[0].size - 2
-    height = grid.size - 2
-
-    enemy = grid[unit_y][unit_x] == 'E' ? 'G' : 'E'
-    victory = true
-    (1..height).each do |target_y|
-        (1..width).each do |target_x|
-            next unless grid[target_y][target_x] == enemy
-            victory = false
-
-            return [false, victory] if target_x == unit_x && target_y - 1 == unit_y
-            return [false, victory] if target_x - 1 == unit_x && target_y == unit_y
-            return [false, victory] if target_x + 1 == unit_x && target_y == unit_y
-            return [false, victory] if target_x == unit_x && target_y + 1 == unit_y
-
-            grid[target_y - 1][target_x] = '?' if grid[target_y - 1][target_x] == '.'
-            grid[target_y][target_x - 1] = '?' if grid[target_y][target_x - 1] == '.'
-            grid[target_y][target_x + 1] = '?' if grid[target_y][target_x + 1] == '.'
-            grid[target_y + 1][target_x] = '?' if grid[target_y + 1][target_x] == '.'
+cave = []
+units = []
+y = 0
+while input_row = gets
+    cave_row = []
+    input_row.chomp.chars.each_with_index do |tile, x|
+        if tile == 'G'
+            unit = Unit.new
+            unit.team = :goblins
+            unit.x = x
+            unit.y = y
+            unit.hitpoints = 200
+            units << unit
         end
+
+        if tile == 'E'
+            unit = Unit.new
+            unit.team = :elves
+            unit.x = x
+            unit.y = y
+            unit.hitpoints = 200
+            units << unit
+        end
+
+        cave_row << tile
+    end
+    
+    cave << cave_row
+    y += 1
+end
+
+width = cave[0].size - 2
+height = cave.size - 2
+
+def enemy_tile(team)
+    team == :goblins ? 'E' : 'G'
+end
+
+def enemy_adjacent(cave, team, y, x)
+    return true if cave[y - 1][x] == enemy_tile(team)
+    return true if cave[y][x - 1] == enemy_tile(team)
+    return true if cave[y][x + 1] == enemy_tile(team)
+    return true if cave[y + 1][x] == enemy_tile(team)
+    false
+end
+
+def draw_cave(cave, unit = nil, target = nil)
+    cave.each_with_index do |row, y|
+        output = row.map.with_index do |tile, x|
+            y == unit.y && x == unit.x ? "\033[32m#{tile}\033[0m" : tile
+        end
+        output = output.map.with_index do |tile, x|
+            y == target[0] && x == target[1] ? "\033[31m#{tile}\033[0m" : tile
+        end
+        puts output.join('')
     end
 
-    [true, victory]
+    # sleep 1
 end
 
-def unmark_targets(grid)
-    width = grid[0].size - 2
-    height = grid.size - 2
-
-    (1..height).each do |target_y|
-        (1..width).each do |target_x|
-            grid[target_y][target_x] = '.' if grid[target_y][target_x] == '?'
-        end
-    end
-end
-
-def to_id(grid, x, y)
-    width = grid[0].size
-    y * width + x
-end
-
-def to_coords(grid, id)
-    width = grid[0].size
-    [id % width, id / width]
-end
-
-rounds = 0
-pp grid
+rounds = 1
+pp cave
 loop do
-    pp ['round', rounds]
-    moved = Set.new
+    # break if rounds == 3
+    puts "round #{rounds}"
+
+    units = units.sort_by do |unit|
+        unit.y * width + unit.x
+    end
+
     victory = false
-    (1..height).each do |unit_y|
-        (1..width).each do |unit_x|
-            next unless 'EG'.include?(grid[unit_y][unit_x])
+    units.each do |unit|
+        next if unit.hitpoints <= 0
 
-            unit = to_id(grid, unit_x, unit_y)
-            next if moved.include?(unit)
+        targets = units.select do |candidate|
+            candidate.team != unit.team && candidate.hitpoints > 0
+        end
+        
+        if targets.size == 0
+            victory = true
+            break
+        end
 
-            move, victory = mark_targets(grid, unit_x, unit_y)
-            # pp ['*', unit_x, unit_y, move]
-            # pp grid
-            moved_x = unit_x
-            moved_y = unit_y
-            if move
-                # move
-                # pp ['moving', [unit_x, unit_y]]
-                queue = [unit]
-                previous = {}
-                seen = Set.new
-                candidates = []
-                loop do
-                    # pp queue.map { |item| to_coords(grid, item) }
-                    if queue.empty?
-                        puts '$$$'
-                        candidates = candidates.take_while do |candidate|
-                            tile_0, distance_0 = candidates[0]
-                            tile, distance = candidate
-                            distance == distance_0
-                        end
+        # mark in range
+        targets.each do |target|
+            cave[target.y - 1][target.x] = '?' if cave[target.y - 1][target.x] == '.'
+            cave[target.y][target.x - 1] = '?' if cave[target.y][target.x - 1] == '.'
+            cave[target.y][target.x + 1] = '?' if cave[target.y][target.x + 1] == '.'
+            cave[target.y + 1][target.x] = '?' if cave[target.y + 1][target.x] == '.'
+        end
 
-                        break if candidates.empty?
+        # choose move
+        chosen = nil
+        chosen = [unit.y, unit.x, nil, 0] if enemy_adjacent(cave, unit.team, unit.y, unit.x)
+        if unit.y == 11 && unit.x == 19 && unit.team == :elves
+            puts 'CHECK'
+            pp unit, chosen, enemy_adjacent(cave, unit.team, unit.y, unit.x)
+        end
 
-                        candidates = candidates.map do |candidate|
-                            tile, distance = candidate
-                            tile
-                        end
+        visited = Set.new
+        queue = [[unit.y, unit.x, nil, 0]]
+        visited.add([unit.y, unit.x])
+        until !chosen.nil? || queue.empty?
+            current = queue.shift
+            # if cave[current[0]][current[1]] == '?' && enemy_adjacent(cave, unit.team, current[0], current[1])
+            if cave[current[0]][current[1]] == '?'
+                chosen = current
+            end
 
-                        # puts '***'
-                        tile = candidates.min
-                        tile_x, tile_y = to_coords(grid, tile)
-                        grid[tile_y][tile_x] = grid[unit_y][unit_x]
-                        grid[unit_y][unit_x] = '.'
-                        hitpoints[tile_y][tile_x] = hitpoints[unit_y][unit_x]
-                        moved_x = tile_x
-                        moved_y = tile_y
-                        moved.add(tile)
-                        break
-                    end
-
-                    tile = queue.shift
-                    tile_x, tile_y = to_coords(grid, tile)
-                    # pp [tile_x, tile_y]
-                    if grid[tile_y][tile_x] == '?'
-                        distance = 1
-                        current = tile
-                        loop do
-                            ancestor = previous[current]
-                            # pp to_coords(grid, ancestor)
-                            break if ancestor == unit
-                            current = ancestor
-                            distance += 1
-                        end
-
-                        candidates << [tile, distance] unless candidates.include?([tile, distance])
-                        pp ['!', candidates.size, queue.size]
-                    end
-                    pp ['?', tile]
-                    seen.add(tile)
-
-                    step = to_id(grid, tile_x, tile_y - 1)
-                    if (grid[tile_y - 1][tile_x] == '.' || grid[tile_y - 1][tile_x] == '?') && !seen.include?(step)
-                        queue << step unless queue.include?(step)
-                        previous[step] = tile if previous[step].nil? || tile < previous[step]
-                    end
-                    step = to_id(grid, tile_x - 1, tile_y)
-                    if (grid[tile_y][tile_x - 1] == '.' || grid[tile_y][tile_x - 1] == '?') && !seen.include?(step)
-                        queue << step unless queue.include?(step)
-                        previous[step] = tile if previous[step].nil? || tile < previous[step]
-                    end
-                    step = to_id(grid, tile_x + 1, tile_y)
-                    if (grid[tile_y][tile_x + 1] == '.' || grid[tile_y][tile_x + 1] == '?') && !seen.include?(step)
-                        queue << step unless queue.include?(step)
-                        previous[step] = tile if previous[step].nil? || tile < previous[step]
-                    end
-                    step = to_id(grid, tile_x, tile_y + 1)
-                    if (grid[tile_y + 1][tile_x] == '.' || grid[tile_y + 1][tile_x] == '?') && !seen.include?(step)
-                        queue << step unless queue.include?(step)
-                        previous[step] = tile if previous[step].nil? || tile < previous[step]
-                    end
-                    pp ['!', candidates.size, queue.size, queue[0], seen.include?(queue[0])]
+            if '.?'.include?(cave[current[0] - 1][current[1]]) && !visited.include?([current[0] - 1, current[1]])
+                if current[2].nil?
+                    queue << [current[0] - 1, current[1], :north, current[3] + 1]
+                else
+                    queue << [current[0] - 1, current[1], current[2], current[3] + 1]
                 end
-            end
-            unmark_targets(grid)
-            # pp grid
-
-            # attack
-            # pp ['attcking', [unit_x, unit_y]]
-            enemy = grid[moved_y][moved_x] == 'E' ? 'G' : 'E'
-            targets = []
-            targets << [moved_x, moved_y - 1, 0] if grid[moved_y - 1][moved_x] == enemy
-            targets << [moved_x - 1, moved_y, 1] if grid[moved_y][moved_x - 1] == enemy
-            targets << [moved_x + 1, moved_y, 2] if grid[moved_y][moved_x + 1] == enemy
-            targets << [moved_x, moved_y + 1, 3] if grid[moved_y + 1][moved_x] == enemy
-            targets = targets.sort_by do |target|
-                target_x, target_y, priority = target
-                hitpoints[target_y][target_x] * 4 + priority
+                visited.add([current[0] - 1, current[1]])
             end
 
-            # puts '******'
-            # pp grid, [moved_x, moved_y, hitpoints[moved_y][moved_x], targets]
-            # puts '******'
-            unless targets.empty?
-                target_x, target_y, _ = targets.shift
-                hitpoints[target_y][target_x] -= 3
-                grid[target_y][target_x] = '.' if hitpoints[target_y][target_x] <= 0
+            if '.?'.include?(cave[current[0]][current[1] - 1]) && !visited.include?([current[0], current[1] - 1])
+                if current[2].nil?
+                    queue << [current[0], current[1] - 1, :west, current[3] + 1]
+                else
+                    queue << [current[0], current[1] - 1, current[2], current[3] + 1]
+                end
+                visited.add([current[0], current[1] - 1])
+            end
+
+            if '.?'.include?(cave[current[0]][current[1] + 1]) && !visited.include?([current[0], current[1] + 1])
+                if current[2].nil?
+                    queue << [current[0], current[1] + 1, :east, current[3] + 1]
+                else
+                    queue << [current[0], current[1] + 1, current[2], current[3] + 1]
+                end
+                visited.add([current[0], current[1] + 1])
+            end
+
+            if '.?'.include?(cave[current[0] + 1][current[1]]) && !visited.include?([current[0] + 1, current[1]])
+                if current[2].nil?
+                    queue << [current[0] + 1, current[1], :south, current[3] + 1]
+                else
+                    queue << [current[0] + 1, current[1], current[2], current[3] + 1]
+                end
+                visited.add([current[0] + 1, current[1]])
+            end
+        end
+        pp unit, chosen
+        # queue = queue.select do |candidate|
+        #     cave[candidate[0]][candidate[1]] == '?' && !chosen.nil? && candidate[2] != chosen[2] && candidate[3] == chosen[3]
+        # end
+        # pp queue
+
+        # move
+        unless chosen.nil? || chosen[2].nil?
+            # puts "\033[2J"
+            puts "unit moves #{chosen[2].to_s}"
+            draw_cave(cave, unit, [chosen[0], chosen[1]])
+
+            cave[unit.y][unit.x] = '.'
+
+            unit.y -= 1 if chosen[2] == :north
+            unit.x -= 1 if chosen[2] == :west
+            unit.x += 1 if chosen[2] == :east
+            unit.y += 1 if chosen[2] == :south
+
+            cave[unit.y][unit.x] = 'G' if unit.team == :goblins
+            cave[unit.y][unit.x] = 'E' if unit.team == :elves
+
+            # puts "\033[2J"
+            puts "unit moved"
+            draw_cave(cave, unit, [chosen[0], chosen[1]])
+        end
+        
+        # unmark in range
+        cave.map! do |row|
+            row.map! do |tile|
+                tile == '?' ? '.' : tile
+            end
+        end
+
+        # chose attack target
+        targets = units.select do |candidate|
+            candidate.team != unit.team && candidate.hitpoints > 0
+        end
+        
+        targets = targets.select do |candidate|
+            (candidate.x - unit.x).abs + (candidate.y - unit.y).abs == 1
+        end
+
+        targets = targets.sort_by do |candidate|
+            candidate.hitpoints * width * height + candidate.y * width + candidate.x
+        end
+
+        # attack
+        unless targets.empty?
+            target = targets.shift
+
+            # puts "\033[2J"
+            puts "unit attacks"
+            draw_cave(cave, unit, [target.y, target.x])
+
+            target.hitpoints -= 3
+            if target.hitpoints <= 0
+                cave[target.y][target.x] = '.'
             end
         end
     end
 
-    pp grid
-    # elves = 0
-    # goblins = 0
-    remaining_hitpoints = 0
-    (1..height).each do |unit_y|
-        (1..width).each do |unit_x|
-            next unless 'EG'.include?(grid[unit_y][unit_x])
-            pp [unit_x, unit_y, hitpoints[unit_y][unit_x]]
-            # elves += 1 if grid[unit_y][unit_x] == 'E'
-            # goblins += 1 if grid[unit_y][unit_x] == 'G'
-            remaining_hitpoints += hitpoints[unit_y][unit_x]
-        end
-    end
-
-    # if elves == 0 || goblins == 0
     if victory
-        pp [rounds, remaining_hitpoints, rounds * remaining_hitpoints]
+        units = units.sort_by do |unit|
+            unit.y * width + unit.x
+        end
+        pp units
+
+        hitpoints = units.map do |unit|
+            [unit.hitpoints, 0].max
+        end
+        total = hitpoints.sum
+        puts rounds - 1, total, (rounds - 1) * total
         break
     end
+
+    # cave.each do |row|
+    #     pp row.join('')
+    # end
+    # units = units.sort_by do |unit|
+    #     unit.y * width + unit.x
+    # end
+    # pp units
 
     rounds += 1
 end
