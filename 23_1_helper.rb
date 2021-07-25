d11a8efa49f0197a28be1164a9c49722b7eaa4ca
Marc Dominik Migge <marcmigge@gmx.net>
2021-07-25 10:59:44 +0200

    Day 23, part 2 experiments

--- 23_1_helper.rb
+++ 23_1_helper.rb
@@ -4,6 +4,14 @@ def manhattan(coords_a, coords_b)
     (coords_a[0] - coords_b[0]).abs + (coords_a[1] - coords_b[1]).abs
 end
 
+def quadrant_pp(coords_a, coords_b)
+    (coords_a[0] - coords_b[0]) + (coords_a[1] - coords_b[1])
+end
+
+def quadrant_pp_moved(coords_a, coords_b)
+    coords_a[0] + coords_a[1]
+end
+
 
 grid = []
 20.times { grid << ('.' * 20) }
@@ -14,6 +22,56 @@ bots << [[11, 10], 8]
 20.times do |y|
     20.times do |x|
         covered = bots.count { |bot| manhattan([x, y], bot[0]) <= bot[1] }
+        case covered
+        when 0
+            grid[y][x] = '.'
+        when 1
+            grid[y][x] = '+'
+        when 2
+            grid[y][x] = '#'
+        end
+    end
+end
+
+
+puts grid
+puts
+
+
+# grid = []
+# 20.times { grid << ('.' * 20) }
+
+# bots = []
+# bots << [[7, 7], 5]
+# bots << [[11, 10], 8]
+# 20.times do |y|
+#     20.times do |x|
+#         covered = bots.count { |bot| quadrant_pp([x, y], bot[0]) == bot[1] }
+#         case covered
+#         when 0
+#             grid[y][x] = '.'
+#         when 1
+#             grid[y][x] = '+'
+#         when 2
+#             grid[y][x] = '#'
+#         end
+#     end
+# end
+
+
+# puts grid
+# puts
+
+
+grid = []
+20.times { grid << ('.' * 20) }
+
+bots = []
+bots << [[7, 7], 5]
+bots << [[11, 10], 8]
+20.times do |y|
+    20.times do |x|
+        covered = bots.count { |bot| -x - y == bot[1] - bot[0][0] - bot[0][1] }
         case covered
         when 0
             grid[y][x] = '.'
