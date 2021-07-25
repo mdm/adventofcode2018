98427886a13203276322628a6131d8e9892f3c31
Marc Dominik Migge <marcmigge@gmx.net>
2021-07-25 14:00:40 +0200

    Finish day 23, part 2

--- 23_1.rb
+++ 23_1.rb
@@ -7,12 +7,12 @@ while bot = gets
     bots << [coords, radius]
 end
 
-pp bots.size
+# pp bots.size
 
 max_radius = bots.map { |bot| bot[1] }.max
 strongest_bot = bots.reject { |bot| bot[1] < max_radius }.first
 
-pp strongest_bot
+# pp strongest_bot
 
 def manhattan(bot_a, bot_b)
     (bot_a[0][0] - bot_b[0][0]).abs + (bot_a[0][1] - bot_b[0][1]).abs + (bot_a[0][2] - bot_b[0][2]).abs
@@ -21,31 +21,67 @@ end
 in_range = bots.count { |bot| manhattan(bot, strongest_bot) <= max_radius }
 puts in_range
 
-x_coords = bots.map { |bot| bot[0][0] }
-y_coords = bots.map { |bot| bot[0][1] }
-z_coords = bots.map { |bot| bot[0][2] }
 
-sizes = [x_coords.max -  x_coords.min, y_coords.max -  y_coords.min, z_coords.max -  z_coords.min]
+bot_ranges = bots.map do |bot|
+    [
+        bot[1] + bot[0][0] + bot[0][1] + bot[0][2], # 0 opposite to 6
+        bot[1] - bot[0][0] + bot[0][1] + bot[0][2], # 1 opposite to 7
+        bot[1] - bot[0][0] - bot[0][1] + bot[0][2], # 2 opposite to 4
+        bot[1] + bot[0][0] - bot[0][1] + bot[0][2], # 3 opposite to 5
+        bot[1] + bot[0][0] + bot[0][1] - bot[0][2], # 4 opposite to 2
+        bot[1] - bot[0][0] + bot[0][1] - bot[0][2], # 5 opposite to 3
+        bot[1] - bot[0][0] - bot[0][1] - bot[0][2], # 6 opposite to 0
+        bot[1] + bot[0][0] - bot[0][1] - bot[0][2], # 7 opposite to 1
+    ]
+end
 
-pp sizes, sizes.reduce(:*)
+def intersect?(ranges_a, ranges_b)
+    axis_1 = -ranges_a[6] <= ranges_b[0] && -ranges_b[6] <= ranges_a[0]
+    axis_2 = -ranges_a[7] <= ranges_b[1] && -ranges_b[7] <= ranges_a[1]
+    axis_3 = -ranges_a[4] <= ranges_b[2] && -ranges_b[4] <= ranges_a[2]
+    axis_4 = -ranges_a[5] <= ranges_b[3] && -ranges_b[5] <= ranges_a[3]
 
-candidates = bots.each_with_index.map { |bot, i| [i] }
+    axis_1 && axis_2 && axis_3 && axis_4
+end
 
-bots.size.times do
-    next_candidates = []
-    candidates.each do |candidate|
-        bots.each_with_index do |bot, bot_i|
-            intersecting = candidate.all? do |candidate_bot|
-                bot[1] + candidate_bot[1] >= manhattan(bot, candidate_bot)
-            end
-            if intersecting
-                candidate << bot_i
-                next_candidates << candidate
-            end
+def inside?(ranges, x, y, z)
+    result   =  x + y + z <= ranges[0]
+    result &&= -x + y + z <= ranges[1]
+    result &&= -x - y + z <= ranges[2]
+    result &&=  x - y + z <= ranges[3]
+    result &&=  x + y - z <= ranges[4]
+    result &&= -x + y - z <= ranges[5]
+    result &&= -x - y - z <= ranges[6]
+    result &&=  x - y - z <= ranges[7]
+end
+
+max_in_range = 0
+argmax_in_range = []
+bot_ranges.each do |ranges_a|
+    intersection = ranges_a
+    in_range = 1
+    bot_ranges.each do |ranges_b|
+        if intersect?(intersection, ranges_b)
+            intersection = [
+                intersection[0] < ranges_b[0] ? intersection[0] : ranges_b[0],
+                intersection[1] < ranges_b[1] ? intersection[1] : ranges_b[1],
+                intersection[2] < ranges_b[2] ? intersection[2] : ranges_b[2],
+                intersection[3] < ranges_b[3] ? intersection[3] : ranges_b[3],
+                intersection[4] < ranges_b[4] ? intersection[4] : ranges_b[4],
+                intersection[5] < ranges_b[5] ? intersection[5] : ranges_b[5],
+                intersection[6] < ranges_b[6] ? intersection[6] : ranges_b[6],
+                intersection[7] < ranges_b[7] ? intersection[7] : ranges_b[7],
+            ]
+            in_range += 1
         end
     end
 
-    # break if next_candidates.size == candidates.size 
-    pp next_candidates
-    candidates = next_candidates
+    # pp ranges_a, intersection, in_range, inside?(intersection, 12, 12, 12)
+    if in_range >= max_in_range
+        max_in_range = in_range
+        argmax_in_range << intersection
+    end
 end
+
+# pp max_in_range, argmax_in_range
+puts argmax_in_range[0][0]
--- 23_1_helper.rb
+++ 23_1_helper.rb
@@ -17,6 +17,7 @@ grid = []
 20.times { grid << ('.' * 20) }
 
 bots = []
+# bots << [[0, 0], 5]
 bots << [[7, 7], 5]
 bots << [[11, 10], 8]
 20.times do |y|
@@ -38,15 +39,61 @@ puts grid
 puts
 
 
-# grid = []
-# 20.times { grid << ('.' * 20) }
+grid = []
+20.times { grid << ('.' * 20) }
 
-# bots = []
-# bots << [[7, 7], 5]
+bots = []
+# bots << [[0, 0], 5]
+bots << [[0, 0], 3]
+bots << [[0, 0], 5]
 # bots << [[11, 10], 8]
+half_planes = bots.map do |bot|
+    [
+        bot[1] + bot[0][0] + bot[0][1],
+        bot[1] - bot[0][0] + bot[0][1],
+        bot[1] - bot[0][0] - bot[0][1],
+        bot[1] + bot[0][0] - bot[0][1],
+    ]
+end
+
+intersection = if -half_planes[0][2] <= half_planes[1][0] && -half_planes[1][2] <= half_planes[0][0] && -half_planes[0][3] <= half_planes[1][1] && -half_planes[1][3] <= half_planes[0][1]
+    [
+        half_planes[0][0] < half_planes[1][0] ? half_planes[0][0] : half_planes[1][0],
+        half_planes[0][1] < half_planes[1][1] ? half_planes[0][1] : half_planes[1][1],
+        half_planes[0][2] < half_planes[1][2] ? half_planes[0][2] : half_planes[1][2],
+        half_planes[0][3] < half_planes[1][3] ? half_planes[0][3] : half_planes[1][3],
+    ]
+end
+pp intersection
+
+half_planes << intersection
+
+20.times do |y|
+    20.times do |x|
+        covered = half_planes.count { |half_plane| x + y <= half_plane[0] && -x + y <= half_plane[1] && -x - y <= half_plane[2] && x - y <= half_plane[3] }
+        case covered
+        when 0
+            grid[y][x] = '.'
+        when 1
+            grid[y][x] = '+'
+        when 2
+            grid[y][x] = '#'
+        when 3
+            grid[y][x] = '*'
+        end
+    end
+end
+
+
+puts grid
+
+# pp bots.map { |bot| bot[1] + bot[0][0] + bot[0][1] }
+# pp bots.map { |bot| bot[1] - bot[0][0] + bot[0][1] }
+# pp bots.map { |bot| bot[1] - bot[0][0] - bot[0][1] }
+# pp bots.map { |bot| bot[1] + bot[0][0] - bot[0][1] }
 # 20.times do |y|
 #     20.times do |x|
-#         covered = bots.count { |bot| quadrant_pp([x, y], bot[0]) == bot[1] }
+#         covered = bots.count { |bot| x - y <= bot[1] + bot[0][0] - bot[0][1] }
 #         case covered
 #         when 0
 #             grid[y][x] = '.'
@@ -60,28 +107,3 @@ puts
 
 
 # puts grid
-# puts
-
-
-grid = []
-20.times { grid << ('.' * 20) }
-
-bots = []
-bots << [[7, 7], 5]
-bots << [[11, 10], 8]
-20.times do |y|
-    20.times do |x|
-        covered = bots.count { |bot| -x - y == bot[1] - bot[0][0] - bot[0][1] }
-        case covered
-        when 0
-            grid[y][x] = '.'
-        when 1
-            grid[y][x] = '+'
-        when 2
-            grid[y][x] = '#'
-        end
-    end
-end
-
-
-puts grid
